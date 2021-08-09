d1c2a24d546c1a3c0b0edc1c2b288ee9ba43b204
MarianaNVasquez <mariana.norenav@udea.edu.co>
2021-08-09 17:00:33 -0500

    Se agrega la función walkPlayer, la cual, permite ir cambiando los sprites según la tecla presionada.

--- theguy.cpp
+++ theguy.cpp
@@ -5,8 +5,17 @@ theguy::theguy()
     Spown = new QTimer(this);
     Death = new QTimer(this);
 
+    Spown->stop();
+    Death->stop();
+
     mMoveX = false;
     mMoveY = false;
+    mJump = false; //Cuando el personaje salte no cambie las imagenes a mostar sino que solo muestre la de salto
+
+    Vx = 0;
+    Vy = 0;
+    Ax = 0;
+    Ay = 0;
 
     for(int f = 0; f < 9; f++){
         mMove[f] = 0;
@@ -14,11 +23,73 @@ theguy::theguy()
     }
 
     connect(Spown, SIGNAL(timeout()), this, SLOT(updateSpown()));
+    connect(Death,SIGNAL(timeout()),this,SLOT(updateDeath()));
+}
+
+void theguy::walkPlayer(short a)
+{
+    switch(a){
+    case 'W':
+
+        if(mMoveX){
+            if(mMove[6] == 1){
+                mCurrentImag[6] = false;
+            }
+            else if(mMove[6] == 0){
+                mCurrentImag[6] = true;
+            }
+
+            mMove[6] += (2*mCurrentImag[6])-1;
+            setPixmap(QPixmap(mJumpRight[mMove[6]]).scaled(50,50));
+        }
+        else{
+            if(mMoveY){
+                if(mMove[7] == 1){
+                    mCurrentImag[7] = false;
+                }
+                else if(mMove[7] == 0){
+                    mCurrentImag[7] = true;
+                }
+
+                mMove[7] += (2*mCurrentImag[7])-1;
+                setPixmap(QPixmap(mJumpLeft[mMove[7]]).scaled(50,50));
+            }
+        }
+
+        break;
+
+    case 'A':
+
+        if(mMove[1] == 2){
+            mCurrentImag[1] = false;
+        }
+        else if(mMove[1] == 0){
+            mCurrentImag[1] = true;
+        }
+
+        mMove[1] += (2*mCurrentImag[1])-1;
+        setPixmap(QPixmap(mMoveLeft[mMove[1]]).scaled(50,50));
+
+        break;
+
+    case 'D':
+
+        if(mMove[0] == 2){
+            mCurrentImag[0] = false;
+        }
+        else if(mMove[0] == 0){
+            mCurrentImag[0] = true;
+        }
+
+        mMove[0] += (2*mCurrentImag[0])-1;
+        setPixmap(QPixmap(mMoveRight[mMove[0]]).scaled(50,50));
+        break;
+    }
 }
 
 void theguy::updateSpown()
 {
-    setPixmap(QPixmap(mSpown[mMove[8]]).scaled(35,35));
+    setPixmap(QPixmap(mSpown[mMove[8]]).scaled(50,50));
 
     if(mMove[8] == 5){
         Spown->stop();
@@ -30,7 +101,7 @@ void theguy::updateSpown()
 void theguy::updateDeath()
 {
     if(mMoveX){
-        setPixmap(QPixmap(mDeathRight[mMove[4]]).scaled(35,35));
+        setPixmap(QPixmap(mDeathRight[mMove[4]]).scaled(50,50));
 
         if(mMove[4] == 4){
             Spown->stop();
@@ -40,7 +111,7 @@ void theguy::updateDeath()
     }
     else{
         if(mMoveY){
-            setPixmap(QPixmap(mDeathLeft[mMove[5]]).scaled(35,35));
+            setPixmap(QPixmap(mDeathLeft[mMove[5]]).scaled(50,50));
 
             if(mMove[5] == 4){
                 Spown->stop();
@@ -50,3 +121,14 @@ void theguy::updateDeath()
         }
     }
 }
+
+QTimer *theguy::getDeath() const
+{
+    return Death;
+}
+
+QTimer *theguy::getSpown() const
+{
+    return Spown;
+}
+
--- theguy.h
+++ theguy.h
@@ -14,6 +14,31 @@ class theguy: public QObject, public QGraphicsPixmapItem
 public:
 
     theguy();
+    void walkPlayer(short a);
+
+    float getPosX() const {return posX;}
+    float getPosY() const {return posY;}
+    float getVx() const {return Vx;}
+    float getVy() const {return Vy;}
+    float getAx() const {return Ax;}
+    float getAy() const {return Ay;}
+    bool getMoveX() const {return mMoveX;}
+    bool getMoveY() const {return mMoveY;}
+
+
+    void setPosX(float value) {posX = value;}
+    void setPosY(float value) {posY = value;}
+    void setVx(float value) {Vx = value;}
+    void setVy(float value) {Vy = value;}
+    void setAx(float value) {Ax = value;}
+    void setAy(float value) {Ay = value;}
+    void setMoveX(bool moveX) {mMoveX = moveX;}
+    void setMoveY(bool moveY) {mMoveY = moveY;}
+
+
+    QTimer *getSpown() const;
+
+    QTimer *getDeath() const;
 
 public slots:
 
@@ -22,23 +47,23 @@ public slots:
 
 private:
 
-    QString mMoveRight[4] = {":/Imagenes/The guy sprites/Movimiento/Derecha1.png",":/Imagenes/The guy sprites/Movimiento/Derecha2.png",":/Imagenes/The guy sprites/Movimiento/Derecha3.png",":/Imagenes/The guy sprites/Movimiento/Derecha4.png"};
-    QString mMoveLeft[4] = {":/Imagenes/The guy sprites/Movimiento/Izquierda1.png",":/Imagenes/The guy sprites/Movimiento/Izquierda2.png",":/Imagenes/The guy sprites/Movimiento/Izquierda3.png",":/Imagenes/The guy sprites/Movimiento/Izquierda4.png"};
+    QString mMoveRight[3] = {":/Imagenes/The guy sprites/Movimiento/Derecha2.png",":/Imagenes/The guy sprites/Movimiento/Derecha3.png",":/Imagenes/The guy sprites/Movimiento/Derecha4.png"};
+    QString mMoveLeft[3] = {":/Imagenes/The guy sprites/Movimiento/Izquierda2.png",":/Imagenes/The guy sprites/Movimiento/Izquierda3.png",":/Imagenes/The guy sprites/Movimiento/Izquierda4.png"};
     QString mAttackRight[4] = {":/Imagenes/The guy sprites/Ataque/Ataque_derecha1.png",":/Imagenes/The guy sprites/Ataque/Ataque_derecha2.png"};
     QString mAttackLeft[4] = {":/Imagenes/The guy sprites/Ataque/Ataque_izquierda1.png",":/Imagenes/The guy sprites/Ataque/Ataque_izquierda2.png"};
     QString mDeathRight[5] = {":/Imagenes/The guy sprites/Muerte/Muerte_derecha1.png",":/Imagenes/The guy sprites/Muerte/Muerte_derecha2.png",":/Imagenes/The guy sprites/Muerte/Muerte_derecha3.png",":/Imagenes/The guy sprites/Muerte/Muerte_derecha4.png",":/Imagenes/The guy sprites/Muerte/Muerte_derecha5.png"};
     QString mDeathLeft[5] = {":/Imagenes/The guy sprites/Muerte/Muerte_izquierda1.png",":/Imagenes/The guy sprites/Muerte/Muerte_izquierda2.png",":/Imagenes/The guy sprites/Muerte/Muerte_izquierda3.png",":/Imagenes/The guy sprites/Muerte/Muerte_izquierda4.png",":/Imagenes/The guy sprites/Muerte/Muerte_izquierda5.png"};
-    QString mJumpRight[3] = {":/Imagenes/The guy sprites/Salto/Salto_derecha1.png",":/Imagenes/The guy sprites/Salto/Salto_derecha2.png",":/Imagenes/The guy sprites/Salto/Salto_derecha3.png"};
-    QString mJumpLeft[3] = {":/Imagenes/The guy sprites/Salto/Salto_izquierda1.png",":/Imagenes/The guy sprites/Salto/Salto_izquierda2.png",":/Imagenes/The guy sprites/Salto/Salto_izquierda3.png"};
+    QString mJumpRight[2] = {":/Imagenes/The guy sprites/Salto/Salto_derecha1.png",":/Imagenes/The guy sprites/Salto/Salto_derecha2.png"};
+    QString mJumpLeft[2] = {":/Imagenes/The guy sprites/Salto/Salto_izquierda1.png",":/Imagenes/The guy sprites/Salto/Salto_izquierda2.png"};
     QString mSpown[6] = {":/Imagenes/The guy sprites/Spown/Spown1.png",":/Imagenes/The guy sprites/Spown/Spown2.png",":/Imagenes/The guy sprites/Spown/Spown3.png",":/Imagenes/The guy sprites/Spown/Spown4.png",":/Imagenes/The guy sprites/Spown/Spown5.png",":/Imagenes/The guy sprites/Spown/Spown6.png"};
 
     QTimer *Spown;
     QTimer *Death;
 
     float posX, posY, Vx, Vy, Ax, Ay;
-    short mMove[9], mMoveX, mMoveY;
-    bool mCurrentImag[9];
-    int mPastImage = -1;
+    short mMove[9];
+    bool mCurrentImag[9], mMoveX, mMoveY;
+    int mPastImage = -1, mVidas;
 
 
 
